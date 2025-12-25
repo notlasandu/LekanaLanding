91b54af4bbafe9d96e89a095974d6c757acd9b64
notlasandu <lazercreaxions@gmail.com>
2025-12-25 11:33:53 +0530

    responsive layout fix

--- src/lib/components/Waitlist.svelte
+++ src/lib/components/Waitlist.svelte
@@ -33,29 +33,28 @@
 		}
 	}
 </script>
-<div class="flex flex-col w-full max-w-sm">
 
-	<div class="flex w-fit rounded-full bg-neutral-950 text-sm text-white">
+<div class="flex w-full max-w-sm flex-col">
+	<div class="flex w-full items-center rounded-full bg-neutral-950 text-sm text-white">
 		<input
-		id="email"
-		name="email"
-		type="email"
-		bind:value={email}
-		placeholder="you@company.com"
-		class=" rounded-full border-neutral-700 px-8 py-1 text-sm transition-all duration-200 outline-none md:py-2.5"
-		required
+			id="email"
+			name="email"
+			type="email"
+			bind:value={email}
+			placeholder="you@company.com"
+			class="w-full flex-1 rounded-full border-neutral-700 px-4 text-sm transition-all duration-200 outline-none md:px-8 py-3"
+			required
 		/>
-	<div class="{loading?'glow-btn': 'bg-neutral-800'} flex w-fit rounded-full p-0.5">
-		<button
-			class="flex gap-2 rounded-full bg-neutral-950 px-6 py-2 shadow-lg transition-shadow duration-500 hover:shadow-green-500/50 md:py-4"
-			onclick={submitToWaitlist}
-		>
-			Join Waitlist
-		</button>
+		<div class="{loading ? 'glow-btn' : 'bg-neutral-800'} mr-0.5 flex w-fit rounded-full p-0.5">
+			<button
+				class="rounded-full bg-neutral-950 px-4 py-3 text-sm whitespace-nowrap shadow-lg transition-shadow duration-500 hover:shadow-green-500/50 md:px-6 md:py-4 md:text-base"
+				onclick={submitToWaitlist}
+			>
+				Join Waitlist
+			</button>
+		</div>
 	</div>
-</div>
-<p class="mt-2 pl-6 text-sm text-white">
-	{message}
-</p>
-
+	<p class="mt-2 pl-6 text-xs text-white/70 md:text-sm">
+		{message}
+	</p>
 </div>
